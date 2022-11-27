0e0b1eac5e81e66306d58bdc7c20ed77ebc9dc29
chauhanaman2748 <spyman2748@gmail.com>
2022-11-26 19:10:38 -0500

    Uwindsor project for child care

--- lib/Dashboard/gridDashboard.dart
+++ lib/Dashboard/gridDashboard.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:unnovate/Child_Diet/diet_nav.dart';
 import 'package:unnovate/child_Profile/child_profile.dart';
-import 'package:unnovate/Child_Diet/child_diet.dart';
 import 'package:unnovate/Room_light/room_light.dart';
 import 'package:unnovate/Room_music/room_music.dart';
-import 'package:unnovate/Child_CCTV/child_camera.dart';
 import 'package:unnovate/Child_lock/room_lock.dart';
 
+import '../Child_Vaccine/vacc_nav.dart';
+
+
+//creating list of items to be displayed on dashboard and items to appear on each tab.
 class GridDashboard extends StatelessWidget {
   final Items item1 = Items(
       title: "Kid's Profile",
@@ -17,7 +20,7 @@ class GridDashboard extends StatelessWidget {
       img: 'assets/dashboard/diet.jpg'
   );
   final Items item3 = Items(
-    title: "Ambience Control",
+    title: "Light Control",
     img: 'assets/dashboard/light.jpg'
   );
   final Items item4 = Items(
@@ -29,20 +32,23 @@ class GridDashboard extends StatelessWidget {
     img: 'assets/dashboard/lock.jpeg'
   );
   final Items item6 = Items(
-    title: "Monitor",
-    img: 'assets/dashboard/camera.png'
+    title: "Kid's Vaccine",
+    img: 'assets/dashboard/vaccine.jpeg'
   );
 
   GridDashboard({super.key});
 
+
+  //listing out navigation from each tab.
   @override
   Widget build(BuildContext context) {
     List<Items> myList = [item1, item2, item3, item4, item5, item6];
+
     return Flexible(
       child: GridView.count(
           childAspectRatio: 1.0,
-          padding: const EdgeInsets.only(left: 16, right: 16),
-          crossAxisCount: 2,
+          padding: const EdgeInsets.only(left: 16, right: 16),                  //original 16,16
+          crossAxisCount: 2,                                                    //original 2
           crossAxisSpacing: 18,
           mainAxisSpacing: 18,
           children: myList.map((data) {
@@ -52,9 +58,9 @@ class GridDashboard extends StatelessWidget {
                     Navigator.push(context, MaterialPageRoute(builder: (context) => const ChildProfile()));
                   }
                   else if(data.title=="Kid's Diet"){
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const ChildDiet()));
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const DietNav()));
                   }
-                  else if(data.title=="Ambience Control"){
+                  else if(data.title=="Light Control"){
                     Navigator.push(context, MaterialPageRoute(builder: (context) => const RoomLight()));
                   }
                   else if(data.title=="Music Control"){
@@ -63,13 +69,13 @@ class GridDashboard extends StatelessWidget {
                   else if(data.title=="Smart Lock"){
                     Navigator.push(context, MaterialPageRoute(builder: (context) => const RoomLock()));
                   }
-                  else if(data.title=="Monitor"){
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const CCTV()));
+                  else if(data.title=="Kid's Vaccine"){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const VacNav()));
                   }
               },
               child: Container(
                 decoration: BoxDecoration(
-                    image: DecorationImage(image: AssetImage(data.img),opacity: 0.4, fit: BoxFit.cover),
+                    image: DecorationImage(image: AssetImage(data.img), fit: BoxFit.cover),
                     borderRadius: BorderRadius.circular(10)),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -80,8 +86,10 @@ class GridDashboard extends StatelessWidget {
                         style: GoogleFonts.openSans(
                             textStyle: const TextStyle(
                                 color: Colors.black,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600)),
+                                fontSize: 23,
+                                fontWeight: FontWeight.bold
+                            )
+                        ),
                       ),
                     ),
                   ],
@@ -93,6 +101,7 @@ class GridDashboard extends StatelessWidget {
   }
 }
 
+//items to be taken in the list.
 class Items {
   String title;
   String img;
